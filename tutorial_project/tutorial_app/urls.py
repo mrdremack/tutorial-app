d46b6027912ee8610ac8cd9acbe737347caaa803
mrdremack <mr.dremack@gmail.com>
2016-10-20 21:04:13 -0400

    databases

--- tutorial_project/tutorial_app/urls.py
+++ tutorial_project/tutorial_app/urls.py
@@ -1,4 +1,19 @@
+from django.conf.urls.static import static
+from django.confi import settings
+
+urlpatterns = patterns('',
+	# Examples:
+	#url(r'^$','tuts718.views.home',name='home')
+	#url(r'^blog/',include('blog.urls')),
+
+	url(r'^admin/', include(admin.site.urls)),
+	url('r^', include(TuTz718.urls)),
+	)
+)+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
 from django.conf.urls import patterns, url 
 from tutorial_app import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'))
+	url(r'^about/', views.about, name="about")
--- tutorial_project/tutorial_app/views.py
+++ tutorial_project/tutorial_app/views.py
@@ -4,5 +4,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello world!")
+    return HttpResponse("Here will be tutorial")
+
+def index(request):
+    context_dict = {'boldmessage':'tutorials here!'}
+    return HttpResponse("Here will be tutorial")
+
+def index(request):
+    context_dict = {'boldmessage':'tutorials here!'}
+    return render(request, 'index.html', context_dict)
+
+def about(request):
+    return HttpResponse("About")
 
